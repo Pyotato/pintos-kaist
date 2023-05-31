9ab85585119bcf2cc32fd1eb6fd0d89769afd84a
Ubuntu <ubuntu@ip-172-31-3-252.ap-northeast-2.compute.internal>
2023-05-31 17:04:55 +0000

    diff

--- include/devices/fixed_point.h
+++ include/devices/fixed_point.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef ARITHMETIC_H
 #define ARITHMETIC_H
 
@@ -9,16 +8,4 @@ int sub_n_x(int x, int n);
 int mul_x_y(int x, int y);
 int div_x_y(int x, int y);
 
-=======
-#ifndef ARITHMETIC_H
-#define ARITHMETIC_H
-
-int integer_to_float(int n);
-int float_to_integer(int x);
-int add_x_n(int x, int n);
-int sub_n_x(int x, int n);
-int mul_x_y(int x, int y);
-int div_x_y(int x, int y);
-
->>>>>>> 17b9e9244acceb84ebf9244258d7f3ea4631f226
 #endif /* threads/fixed_point.h */
