7943e0ba6e363ce2cd76aa770a8138ece0db3f7f
Pyotato <pyolovely01@gmail.com>
2023-06-01 07:51:47 +0900

    merge

--- threads/fixed_point.c
+++ threads/fixed_point.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* We use 17.14 format
  * So p = 17, q = 14
  * And f = 1 << q
@@ -49,55 +48,3 @@ int div_x_y(int x, int y)
 {
     return ((int64_t)x) * F / y;
 }
-=======
-/* We use 17.14 format
- * So p = 17, q = 14
- * And f = 1 << q
- *
- * x, y are floating point numbers
- * n is integer
- *
- * In stdio.c it is mentioned that..
- * We don't support floating-point arithmetic,
-               and %n can be part of a security hole.
- */
-#include <stdio.h>
-#include <stdint.h>
-#include "threads/fixed_point.h"
-
-#define Q 14
-#define F (1 << Q)
-
-int integer_to_float(int n)
-{
-    return n * F;
-}
-
-int float_to_integer(int x)
-{
-    if (x >= 0)
-        return (x + (F / 2)) / F;
-    else
-        return (x - (F / 2)) / F;
-}
-
-int add_x_n(int x, int n)
-{
-    return x + n * F;
-}
-
-int sub_n_x(int x, int n)
-{
-    return x - n * F;
-}
-
-int mul_x_y(int x, int y)
-{
-    return ((int64_t)x) * y / F;
-}
-
-int div_x_y(int x, int y)
-{
-    return ((int64_t)x) * F / y;
-}
->>>>>>> 17b9e9244acceb84ebf9244258d7f3ea4631f226
